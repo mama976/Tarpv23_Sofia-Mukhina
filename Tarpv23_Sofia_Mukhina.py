81d4d9d442bc1158c3f02112586af69d4ffc7307
opilane <opilane@HP-CZC2349HV2>
2023-12-21 12:37:12 +0200

    21.12.2023

--- Tarpv23_Sofia_Mukhina.py
+++ Tarpv23_Sofia_Mukhina.py
@@ -1,1 +1,24 @@
 print("Tere maailm")
+nimi =input("mis on sinu nimi?")
+Vanus=int(iput("Kui vana sa oled?")) #float()->2.5
+print("tere Tulemast! "+nimi+".Sa oled)
+print("tere Tulemast!" ,nimi,).Sa oled",vanus,"aasta vana")
+print ("Tere tulemat {0} Sa oled {1} aastat vana".format(nimi;vanus))
+print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
+
+
+#2
+vanus="Jaak"
+pikkus = 16.5 
+kas_käib_koolis =True
+print("Muutuja vanus=",vanus,"tüüp on",type(vanus))
+print("Muutuja eesnimi=";eesnimi,","tüüp on",type(eesnimi))
+print("Muutuja pikkus=",pikkus,",tüüp on",kas_käin_koolis","tüüp on",type(kas_käin_koolis))
+
+#3
+from random import end=kokku=randint (10,000)
+print("Kokku: ",kokku)
+mitu=int(input("Mitu kommi tahad võtta?"))
+kokku=mitu
+print("Nüüd laua peal on",kokku,"kommid")
+
