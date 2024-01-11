b6795052262fc0a03c21318abe431730da324f3d
opilane <opilane@HP-CZC2349HTS>
2024-01-11 12:27:55 +0200

    project

--- Tarpv23_Sofia_Mukhina.py
+++ Tarpv23_Sofia_Mukhina.py
@@ -1,3 +1,7 @@
+from random import randint
+from sunau import AUDIO_FILE_ENCODING_ADPCM_G721
+
+
 print("Tere maailm")
 nimi =input("mis on sinu nimi?")
 Vanus=int(iput("Kui vana sa oled?")) #float()->2.5
@@ -22,3 +26,28 @@ mitu=int(input("Mitu kommi tahad võtta?"))
 kokku=mitu
 print("Nüüd laua peal on",kokku,"kommid")
 
+#10
+P=randint(1,5)
+hind=12.90
+hind*=1.1 #hind koos jätatega
+osa=round(hind/p,2)
+print("Iga söber maksab: ",osa)
+
+#6
+aeg=float(input("Mitu tundi kulus sõiduks?" ))
+tepikkus= float(input("Mitu kilomeetrit sõitsid? "))
+kiirus = tepikkus/Aeg
+print("Sinu kiirus oli " + str(kiirus) + km/h")
+except:
+ print("Viga andmetüübiga")
+
+ #8
+  @..@
+  (----)
+ ( \__/ )
+ "" ^^
+ #7
+a = int(input ('from'))
+b = (input ('before'))
+c = (sum(range(a+b))) /b
+print (c)
