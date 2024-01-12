f4b9e2da9f31bc338b6fdd615242751c318afc51
opilane <opilane@HP-CZC2349HTS>
2024-01-12 09:50:31 +0200

    project2

--- ProjectSofiaMukhina/ProjectSofiaMukhina.py
+++ ProjectSofiaMukhina/ProjectSofiaMukhina.py
@@ -1,28 +1,28 @@
 from math import *
 print("Ruudu karakteristikud")
-a=float(input('Sisesta ruudu külje pikkus => '))
+a=float(input('Sisesta ruudu külje pikkus => "))
 S=a**2
-print("Ruudu pindala", round(S))
+print("Ruudu pindala", round(S,1))
 P=4*a
-print("Ruudu ümbermõõt", round(P))
+print("Ruudu ümbermõõt", round(P,1))
 di=a*sqrt(2)
 print("Ruudu diagonaal", round(di,2))
 print()
 print("Ristküliku karakteristikud")
-b=float(input("Sisesta ristküliku 1. külje pikkus => ")
-c=foat(input("Sisesta ristküliku 2. külje pikkus => ")
+b=float(input("Sisesta ristküliku 1. külje pikkus => "))
+c=foat(input("Sisesta ristküliku 2. külje pikkus => "))
 S=b*c
-print("Ristküliku pindala", round(S)
+print("Ristküliku pindala", round(S,1))
 P=2*(b+c)
-print("Ristküliku ümbermõõt", round(P))
+print("Ristküliku ümbermõõt", round(P,1))
 di=sqrt(b**2+c**2)
-print("Ristküliku diagonaal", round(di,2))
+print("Ristküliku diagonaal", round(di,1))
 print()
 print("Ringi karakteristikud")
-r=float(tinput("Sisesta ringi raadiusi pikkus => "))
+r=float(tinput("Sisesta ringi raadiusi pikkus =>"))
 d=2*r
-print("Ringi läbimõõt"+ str(d))
+print("Ringi läbimõõt"+ str(d,1)))
 S=pi()*r**2
-print("Ringi pindala", round(S))
-C=2pi()*r
-print("Ringjoone pikkus", round(C)
+print("Ringi pindala", round(S,2))
+C=2*pi*r
+print("Ringjoone pikkus", round(C,2))
